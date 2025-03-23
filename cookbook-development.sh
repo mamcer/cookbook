dca6822596b6198853520d54cfa95f4d8164c2d8
Mario Moreno <m4mc3r@gmail.com>
2025-03-23 15:00:51 -0300

    Add development sh script

---
+++ cookbook-development.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+export SPELLBOOK_API_PORT="5001"
+export SPELLBOOK_WEB_PORT="5000"
+export SPELLBOOK_DB_DRIVER="mysql"
+export SPELLBOOK_DB_USER="root"
+export SPELLBOOK_DB_PASS="root"
+export SPELLBOOK_DB_HOST="localhost"
+export SPELLBOOK_DB_PORT="3366"
+export SPELLBOOK_DB_NAME="cookbook"
